de3bd2f2b9fd0a22e3a89899aea47b7e159f027b
ousttrue <ousttrue@gmail.com>
2022-03-15 19:48:04 +0900

    editor_root

--- pyvim/editor_layout/editor_window/editor_root.py
+++ pyvim/editor_layout/editor_window/editor_root.py
@@ -1,3 +1,4 @@
+from typing import Tuple
 import pathlib
 import sys
 from functools import partial
@@ -6,6 +7,7 @@ import prompt_toolkit.layout
 import prompt_toolkit.filters
 import prompt_toolkit.widgets
 import prompt_toolkit.layout.processors
+from .editor_buffer import EditorBuffer
 
 
 def _try_char(character, backup, encoding=sys.stdout.encoding):
@@ -20,6 +22,127 @@ def _try_char(character, backup, encoding=sys.stdout.encoding):
 
 
 TABSTOP_DOT = _try_char('\u2508', '.')
+
+
+def create_buffer_control(search_control, editor_buffer: EditorBuffer):
+    """
+    Create a new BufferControl for a given location.
+    """
+    @prompt_toolkit.filters.Condition
+    def preview_search():
+        from ...editor import get_editor
+        editor = get_editor()
+        return editor.incsearch
+
+    from ...editor import get_editor
+    editor = get_editor()
+
+    from ..reporting_processor import ReportingProcessor
+    input_processors = [
+        # Processor for visualising spaces. (should come before the
+        # selection processor, otherwise, we won't see these spaces
+        # selected.)
+        prompt_toolkit.layout.processors.ConditionalProcessor(
+            prompt_toolkit.layout.processors.ShowTrailingWhiteSpaceProcessor(),
+            prompt_toolkit.filters.Condition(lambda: editor.display_unprintable_characters)),
+
+        # Replace tabs by spaces.
+        prompt_toolkit.layout.processors.TabsProcessor(
+            tabstop=(lambda: editor.tabstop),
+            char1=(
+                lambda: '|' if editor.display_unprintable_characters else ' '),
+            char2=(lambda: _try_char('\u2508', '.', get_app().output.encoding())
+                   if editor.display_unprintable_characters else ' '),
+        ),
+
+        # Reporting of errors, for Pyflakes.
+        ReportingProcessor(editor_buffer),
+        prompt_toolkit.layout.processors.HighlightSelectionProcessor(),
+        prompt_toolkit.layout.processors.ConditionalProcessor(
+            prompt_toolkit.layout.processors.HighlightSearchProcessor(),
+            prompt_toolkit.filters.Condition(lambda: editor.highlight_search)),
+        prompt_toolkit.layout.processors.ConditionalProcessor(
+            prompt_toolkit.layout.processors.HighlightIncrementalSearchProcessor(),
+            prompt_toolkit.filters.Condition(lambda: editor.highlight_search) & preview_search),
+        prompt_toolkit.layout.processors.HighlightMatchingBracketProcessor(),
+        prompt_toolkit.layout.processors.DisplayMultipleCursors(),
+    ]
+
+    from .lexer import DocumentLexer
+    return prompt_toolkit.layout.BufferControl(
+        lexer=DocumentLexer(editor_buffer),
+        include_default_input_processors=False,
+        input_processors=input_processors,
+        buffer=editor_buffer.buffer,
+        preview_search=preview_search,
+        search_buffer_control=search_control,
+        focus_on_click=True)
+
+
+def create_window_frame(search_control, editor_buffer: EditorBuffer) -> Tuple[prompt_toolkit.layout.containers.HSplit, prompt_toolkit.layout.containers.Window]:
+    """
+    Create a Window for the buffer, with underneat a status bar.
+    """
+    @prompt_toolkit.filters.Condition
+    def wrap_lines():
+        from ...editor import get_editor
+        editor = get_editor()
+        return editor.wrap_lines
+
+    from ...editor import get_editor
+    editor = get_editor()
+
+    def get_line_prefix(buffer, line_number, wrap_count):
+        if wrap_count > 0:
+            result = []
+
+            # Add 'breakindent' prefix.
+            from ...editor import get_editor
+            editor = get_editor()
+            if editor.break_indent:
+                line = buffer.document.lines[line_number]
+                prefix = line[:len(line) - len(line.lstrip())]
+                result.append(('', prefix))
+
+            # Add softwrap mark.
+            result.append(('class:soft-wrap', '...'))
+            return result
+        return ''
+
+    window = prompt_toolkit.layout.Window(
+        create_buffer_control(search_control, editor_buffer),
+        allow_scroll_beyond_bottom=True,
+        scroll_offsets=prompt_toolkit.layout.ScrollOffsets(
+            left=0, right=0,
+            top=(lambda: editor.scroll_offset),
+            bottom=(lambda: editor.scroll_offset)),
+        wrap_lines=wrap_lines,
+        left_margins=[prompt_toolkit.layout.ConditionalMargin(
+            margin=prompt_toolkit.layout.NumberedMargin(
+                display_tildes=True,
+                relative=prompt_toolkit.filters.Condition(lambda: editor.relative_number)),
+            filter=prompt_toolkit.filters.Condition(lambda: editor.show_line_numbers))],
+        cursorline=prompt_toolkit.filters.Condition(
+            lambda: editor.cursorline),
+        cursorcolumn=prompt_toolkit.filters.Condition(
+            lambda: editor.cursorcolumn),
+        colorcolumns=(
+            lambda: [prompt_toolkit.layout.ColorColumn(pos) for pos in editor.colorcolumn]),
+        ignore_content_width=True,
+        ignore_content_height=True,
+        get_line_prefix=partial(get_line_prefix, editor_buffer.buffer))
+
+    from ..window_statusbar import WindowStatusBar
+    from ..window_statusbar_ruler import WindowStatusBarRuler
+
+    return prompt_toolkit.layout.HSplit([
+        window,
+        prompt_toolkit.layout.VSplit([
+            WindowStatusBar(editor, editor_buffer),
+            WindowStatusBarRuler(editor, window,
+                                 editor_buffer.buffer),
+        ], width=prompt_toolkit.layout.Dimension()),  # Ignore actual status bar width.
+    ]), window
 
 
 class EditorRoot:
@@ -95,8 +218,8 @@ class EditorRoot:
                 key = (node, node.editor_buffer)
                 frame = existing_frames.get(key)
                 if frame is None:
-                    frame, pt_window = self._create_window_frame(
-                        node.editor_buffer)
+                    frame, pt_window = create_window_frame(
+                        self.searchline.search_control, node.editor_buffer)
 
                     # Link layout Window to arrangement.
                     node.pt_window = pt_window
@@ -105,10 +228,14 @@ class EditorRoot:
                 return frame
 
             elif isinstance(node, window_arrangement.VSplit):
+                def get_vertical_border_char():
+                    " Return the character to be used for the vertical border. "
+                    return _try_char('\u2502', '|', get_app().output.encoding())
+
                 return prompt_toolkit.layout.VSplit(
                     [create_layout_from_node(n) for n in node],
                     padding=1,
-                    padding_char=self._get_vertical_border_char(),
+                    padding_char=get_vertical_border_char(),
                     padding_style='class:frameborder')
 
             if isinstance(node, window_arrangement.HSplit):
@@ -117,125 +244,3 @@ class EditorRoot:
         layout = create_layout_from_node(
             self.window_arrangement.active_tab.root)
         self.container.content = layout
-
-    def _create_window_frame(self, editor_buffer):
-        """
-        Create a Window for the buffer, with underneat a status bar.
-        """
-        @prompt_toolkit.filters.Condition
-        def wrap_lines():
-            from ...editor import get_editor
-            editor = get_editor()
-            return editor.wrap_lines
-
-        from ...editor import get_editor
-        editor = get_editor()
-        window = prompt_toolkit.layout.Window(
-            self._create_buffer_control(editor_buffer),
-            allow_scroll_beyond_bottom=True,
-            scroll_offsets=prompt_toolkit.layout.ScrollOffsets(
-                left=0, right=0,
-                top=(lambda: editor.scroll_offset),
-                bottom=(lambda: editor.scroll_offset)),
-            wrap_lines=wrap_lines,
-            left_margins=[prompt_toolkit.layout.ConditionalMargin(
-                margin=prompt_toolkit.layout.NumberedMargin(
-                    display_tildes=True,
-                    relative=prompt_toolkit.filters.Condition(lambda: editor.relative_number)),
-                filter=prompt_toolkit.filters.Condition(lambda: editor.show_line_numbers))],
-            cursorline=prompt_toolkit.filters.Condition(
-                lambda: editor.cursorline),
-            cursorcolumn=prompt_toolkit.filters.Condition(
-                lambda: editor.cursorcolumn),
-            colorcolumns=(
-                lambda: [prompt_toolkit.layout.ColorColumn(pos) for pos in editor.colorcolumn]),
-            ignore_content_width=True,
-            ignore_content_height=True,
-            get_line_prefix=partial(self._get_line_prefix, editor_buffer.buffer))
-
-        from ..window_statusbar import WindowStatusBar
-        from ..window_statusbar_ruler import WindowStatusBarRuler
-
-        return prompt_toolkit.layout.HSplit([
-            window,
-            prompt_toolkit.layout.VSplit([
-                WindowStatusBar(editor, editor_buffer),
-                WindowStatusBarRuler(editor, window,
-                                     editor_buffer.buffer),
-            ], width=prompt_toolkit.layout.Dimension()),  # Ignore actual status bar width.
-        ]), window
-
-    def _get_line_prefix(self, buffer, line_number, wrap_count):
-        if wrap_count > 0:
-            result = []
-
-            # Add 'breakindent' prefix.
-            from ...editor import get_editor
-            editor = get_editor()
-            if editor.break_indent:
-                line = buffer.document.lines[line_number]
-                prefix = line[:len(line) - len(line.lstrip())]
-                result.append(('', prefix))
-
-            # Add softwrap mark.
-            result.append(('class:soft-wrap', '...'))
-            return result
-        return ''
-
-    def _get_vertical_border_char(self):
-        " Return the character to be used for the vertical border. "
-        return _try_char('\u2502', '|', get_app().output.encoding())
-
-    def _create_buffer_control(self, editor_buffer):
-        """
-        Create a new BufferControl for a given location.
-        """
-        @prompt_toolkit.filters.Condition
-        def preview_search():
-            from ...editor import get_editor
-            editor = get_editor()
-            return editor.incsearch
-
-        from ...editor import get_editor
-        editor = get_editor()
-
-        from ..reporting_processor import ReportingProcessor
-        input_processors = [
-            # Processor for visualising spaces. (should come before the
-            # selection processor, otherwise, we won't see these spaces
-            # selected.)
-            prompt_toolkit.layout.processors.ConditionalProcessor(
-                prompt_toolkit.layout.processors.ShowTrailingWhiteSpaceProcessor(),
-                prompt_toolkit.filters.Condition(lambda: editor.display_unprintable_characters)),
-
-            # Replace tabs by spaces.
-            prompt_toolkit.layout.processors.TabsProcessor(
-                tabstop=(lambda: editor.tabstop),
-                char1=(
-                    lambda: '|' if editor.display_unprintable_characters else ' '),
-                char2=(lambda: _try_char('\u2508', '.', get_app().output.encoding())
-                       if editor.display_unprintable_characters else ' '),
-            ),
-
-            # Reporting of errors, for Pyflakes.
-            ReportingProcessor(editor_buffer),
-            prompt_toolkit.layout.processors.HighlightSelectionProcessor(),
-            prompt_toolkit.layout.processors.ConditionalProcessor(
-                prompt_toolkit.layout.processors.HighlightSearchProcessor(),
-                prompt_toolkit.filters.Condition(lambda: editor.highlight_search)),
-            prompt_toolkit.layout.processors.ConditionalProcessor(
-                prompt_toolkit.layout.processors.HighlightIncrementalSearchProcessor(),
-                prompt_toolkit.filters.Condition(lambda: editor.highlight_search) & preview_search),
-            prompt_toolkit.layout.processors.HighlightMatchingBracketProcessor(),
-            prompt_toolkit.layout.processors.DisplayMultipleCursors(),
-        ]
-
-        from .lexer import DocumentLexer
-        return prompt_toolkit.layout.BufferControl(
-            lexer=DocumentLexer(editor_buffer),
-            include_default_input_processors=False,
-            input_processors=input_processors,
-            buffer=editor_buffer.buffer,
-            preview_search=preview_search,
-            search_buffer_control=self.searchline.search_control,
-            focus_on_click=True)
